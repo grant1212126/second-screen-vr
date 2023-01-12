dc59bf03ae062753bc483f662162b81aba976bf9
grant1212126 <grant1212126@yahoo.co.uk>
2023-01-12 00:08:30 +0000

    added button scripts

---
+++ Second Screen VR/Assets/Scripts/CanvasControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasControl : MonoBehaviour
+{ 
+    public PauseButton pause;
+    public InterfaceButton nextVideo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
